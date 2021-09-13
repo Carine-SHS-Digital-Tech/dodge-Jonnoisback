edbdac657f3c400f547f8f740c01f82340a77e61
yuvi.khatri <yuvi.khatri@student.education.wa.edu.au>
2021-09-13 15:05:03 +0800

    GitHub

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Basic Pygame Structure
+import time
 
 import pygame                               # Imports pygame and other libraries
 
@@ -7,14 +8,14 @@ import pygame                               # Imports pygame and other libraries
 pygame.init()                               # Pygame is initialised (starts running)
 
 screen = pygame.display.set_mode([700,500]) # Set the width and height of the screen [width,height]
-pygame.display.set_caption("My Game")       # Name your window
+pygame.display.set_caption("Dodge")       # Name your window
+background_image=pygame.image.load("OrchardBackground.jpg").convert()
 done = False                                # Loop until the user clicks the close button.
 clock = pygame.time.Clock()                 # Used to manage how fast the screen updates
 black    = (   0,   0,   0)                 # Define some colors using rgb values.  These can be
 white    = ( 255, 255, 255)                 # used throughout the game instead of using rgb values.
 
 # Define additional Functions and Procedures here
-
 # -------- Main Program Loop -----------
 while done == False:
 
@@ -23,8 +24,8 @@ while done == False:
             done = True                     # Flag that we are done so we exit this loop
 
     # Update sprites here
-
-    pygame.display.flip()                   # Go ahead and update the screen with what we've drawn.
-    clock.tick(20)                          # Limit to 20 frames per second
+screen.blit()
+pygame.display.flip()                   # Go ahead and update the screen with what we've drawn.
+clock.tick(20)                          # Limit to 20 frames per second
 
 pygame.quit()                               # Close the window and quit.
